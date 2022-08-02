ea6343f692e5ef1f36e28c8e97060248dfdeb53a
Andrew Christabel <andrew.christabel@myob.com>
2022-08-02 20:12:22 +1000

    Added coin controller and project dependencies

---
+++ CryptoWebService.API/Controllers/CoinController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWebService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CoinController : Controller
+{
+    public CoinController()
+    {
+        
+    }
+}
